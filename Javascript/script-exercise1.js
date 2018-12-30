f8f6e66732af1497ff2150a83e6546a5122cca27
adoniasvsbarros <adoniasvicente@hotmail.com>
2018-12-30 17:06:27 -0300

    Add Javascript section and first exercise

---
+++ Javascript/script-exercise1.js
@@ -0,0 +1,8 @@
+var firstName = prompt("what is your first name?");
+var lastName = prompt("what is your last name?");
+var age = prompt("what is your age?");
+
+alert("User's full name: " + firstName + " " + lastName);
+console.log("User's full name: " + firstName + " " + lastName);
+alert("User's age: " + age);
+console.log("User's age: " + age);
