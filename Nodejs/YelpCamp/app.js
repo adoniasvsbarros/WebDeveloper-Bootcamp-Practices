08d364f1a67af978f2a70d66ab4ead73cad5ea0e
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-03-27 17:36:07 -0300

    Updating YelpCamp project with user comments

--- Nodejs/YelpCamp/app.js
+++ Nodejs/YelpCamp/app.js
@@ -1,45 +1,113 @@
-// YelpCamp application[
+// YelpCamp application
 
-var express = require('express');
-var app = express();
-var bodyParser = require("body-parser");
+// REQUIREMENTS
+var express     =    require('express'),
+    app         =    express(),
+    bodyParser  =    require("body-parser"),
+    mongoose    =    require("mongoose"),
+    Campground  =    require("./models/campground"),
+    Comment     =    require("./models/comment"),
+    seedDB      =    require("./seeds");
 
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
+app.use(express.static(__dirname + "/public"));
+seedDB();
 
-var campgrounds = [
-    {name: "Jack's Moon", image:"https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104490f2c87ea4e4bdbe_340.jpg"},
-    {name: "Mountain People's Rest", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
-    {name: "Salmon Creek", image:"https://farm9.staticflickr.com/8167/7121865553_e1c6a31f07.jpg"},
-    {name: "Jack's Moon", image:"https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104490f2c87ea4e4bdbe_340.jpg"},
-    {name: "Mountain People's Rest", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
-    {name: "Salmon Creek", image:"https://farm9.staticflickr.com/8167/7121865553_e1c6a31f07.jpg"},
-    {name: "Jack's Moon", image:"https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104490f2c87ea4e4bdbe_340.jpg"},
-    {name: "Mountain People's Rest", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
-    {name: "Salmon Creek", image:"https://farm9.staticflickr.com/8167/7121865553_e1c6a31f07.jpg"}
-]
+// ROUTES
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
+// INDEX - Show all campgrounds
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds",{campgrounds: campgrounds});
+    // Get all campground from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds/index",{campgrounds: allCampgrounds});
+        }
+    });
 });
 
+// CREATE - Add new campground to database
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
+    // Create new campground an save in database
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+    
 });
 
+// NEW - Show form to create new campground
 app.get("/campgrounds/new", function(req, res){
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 
+// SHOW - Show more info about a campground
+app.get("/campgrounds/:id", function(req, res){
+    // find campground with provided id
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(foundCampground)
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+
+// ================================
+// COMMENTS ROUTES
+// ================================
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: foundCampground});
+        }
+    })
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    //lookup campground using id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            redirect("/campgrounds");
+        } else {
+            //create new comment
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    //connect new comment to campground
+                    campground.comments.push(comment);
+                    campground.save();
+                    //redirect to campground show page
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+            
+        }
+    })
+
+});
 
 app.listen(3000,function(){
     console.log('YelpCamp server has started!');
